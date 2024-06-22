e0970b8a737ef4e3aab522e337e8b63b1a2e1fa8
troberts97 <t.roberts@stud.unibas.ch>
2024-06-22 18:30:55 +0200

    Add docker to system boot and add copying of google-id, google-secret and secret into traefik-forward-auth

--- src/docker/setup_compose.sh
+++ src/docker/setup_compose.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-docker network create --driver overlay --attachable monitor-net
-
 start() {
 	docker stack deploy -c $COMPOSE_DIR/"$1"/docker-compose.yml traefik-stack
 }
+
+systemctl enable docker # docker runs on boot
+docker network create --driver overlay --attachable monitor-net
 
 # Ensure script stops on errors and missing env variables
 set -e
@@ -16,6 +17,10 @@ set -e
 COMPOSE_DIR=/opt/docker
 
 cp -R "$SCRIPT_DIR"/compose $COMPOSE_DIR
+
+echo "providers.google.client-id=$GOOGLE_CLIENT_ID" >> $COMPOSE_DIR/forward-auth/traefik-forward-auth
+echo "providers.google.client-secret=$GOOGLE_CLIENT_SECRET" >> $COMPOSE_DIR/forward-auth/traefik-forward-auth
+echo "secret=" >> $COMPOSE_DIR/forward-auth/traefik-forward-auth
 
 start traefik
 start forward-auth

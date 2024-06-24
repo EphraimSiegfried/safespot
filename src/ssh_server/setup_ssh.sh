bd9482e030742165e776947ac11a1e9de1ee5dda
Ephraim Siegfried <114060741+EphraimSiegfried@users.noreply.github.com>
2024-06-24 13:15:53 +0200

    Update setup_ssh.sh

--- src/ssh_server/setup_ssh.sh
+++ src/ssh_server/setup_ssh.sh
@@ -8,6 +8,7 @@ apt-get update && apt-get upgrade -y
 apt-get install openssh-server -y
 
 # Set the configuration for the ssh server
+SCRIPT_DIR=$(dirname $(realpath $0))
 cp $SCRIPT_DIR/sshd.conf /etc/ssh/sshd_config.d/
 
 # Start the ssh server
